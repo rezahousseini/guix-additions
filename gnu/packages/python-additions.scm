d3ab74bc0dea56994ef52512d9f450272d98ca61
Reza Housseini <reza.housseini@gmail.com>
2022-03-02 17:19:58 +0100

    fix: disable sanity checks

--- gnu/packages/python-additions.scm
+++ gnu/packages/python-additions.scm
@@ -191,7 +191,11 @@
         (sha256
           (base32 "1ghwyjl4414sn7d4z8bqfjy526d54hb1zfvlrgbfrnml61rcb3ri"))))
     (build-system python-build-system)
-    (arguments '(#:tests? #f))
+    (arguments
+      (list #:tests? #f
+            #:phases
+            #~(modify-phases %standard-phases
+                (delete 'sanity-check))))
     (propagated-inputs (list python-ruamel.yaml python-sphinx))
     (home-page "https://github.com/Jakski/sphinxcontrib-autoyaml")
     (synopsis
