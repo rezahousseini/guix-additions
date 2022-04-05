dd3fa7aa04670afeba1d906722165a905d9ebb43
Reza Housseini <reza.housseini@gmail.com>
2022-04-05 21:12:24 +0200

    refactor: update native-inputs of package python-tefw-ml

--- gnu/packages/python-additions.scm
+++ gnu/packages/python-additions.scm
@@ -286,12 +286,12 @@ Properties of Water and Steam")
   (let ((commit "19a09946bb65ff9f6ccc09c3e9744951900de857")
         (revision "0")) ;Guix package revision
     (package
-      (name "python-tefw-ml")
-      (version (git-version "0.0.0" revision commit))
-      (source
-       (git-checkout
-	(url "git@gitlab.ost.ch:45022/sciceg/hitachiinovazosen/tefw-ml.git")
-	(commit commit)))
+     (name "python-tefw-ml")
+     (version (git-version "0.0.0" revision commit))
+     (source
+      (git-checkout
+       (url "git@gitlab.ost.ch:45022/sciceg/hitachiinovazosen/tefw-ml.git")
+       (commit commit)))
 					;(source (origin
 					;		(method git-fetch)
 					;		(uri (git-reference
@@ -301,28 +301,38 @@ Properties of Water and Steam")
 					;		(sha256
 					;		 (base32
 					;            "0i8zi228pk8wkyjxdjihdaw0x7zzsdsijiylgzhys51l02fls2jq"))))
-      (build-system python-build-system)
-      (arguments
-       (list #:tests? #f
-             #:phases
-             #~(modify-phases %standard-phases
-		 (delete 'sanity-check))))
-      (native-inputs (list
-		      python-numpy
-		      python-scipy
-		      python-scikit-learn
-		      python-pandas
-		      python-matplotlib
-		      python-pyaml
-		      python-tdigest
-		      python-ttictoc
-		      python-seaborn))
-      (home-page "https://gitlab.ost.ch/sciceg/hitachiinovazosen/tefw-ml")
-      (synopsis
-       "Library for the data-driven modelling and monitoring of boiler signals")
-      (description
-       "Library for the data-driven modelling and monitoring of boiler signals")
-      (license license:expat))))
+     (build-system python-build-system)
+     (arguments
+      `(#:phases
+	(modify-phases %standard-phases
+		       (replace 'check
+				(lambda* (#:key tests? #:allow-other-keys)
+				  (when tests?
+				    (invoke "python" "-m" "unittest")))))))
+     (native-inputs (list
+		     python-numpy
+		     python-scipy
+		     python-scikit-learn
+		     python-pandas
+		     python-matplotlib
+		     python-pyaml
+		     python-tdigest
+		     ;; scripts
+		     python-plac
+		     python-ttictoc
+		     python-seaborn
+		     python-colorama
+ 		     ;; domains
+		     python-iapws
+		     python-pint
+		     ;; testing
+		     python-hypothesis))
+     (home-page "https://gitlab.ost.ch/sciceg/hitachiinovazosen/tefw-ml")
+     (synopsis
+      "Library for the data-driven modelling and monitoring of boiler signals")
+     (description
+      "Library for the data-driven modelling and monitoring of boiler signals")
+     (license license:gpl3))))
 
 (define-public python-questionary
   (package
