7651d062b5eee5e0de508a84f0a00d41a5437517
Reza Housseini <reza@housseini.me>
2023-12-21 12:15:22 +0100

    chore: remove haskell packages moved upstream

--- gnu/packages/haskell-additions.scm
+++ gnu/packages/haskell-additions.scm
@@ -96,30 +96,30 @@ The API is agnostic to your choice of server.")
     (name "ghc-miso")
     (version "1.7.1.0")
     (source
-      (origin
-        (method url-fetch)
-        (uri (string-append
-               "https://hackage.haskell.org/package/miso/miso-"
-               version
-               ".tar.gz"))
-        (sha256
-          (base32
-            "1hkfcinwymrff8mmvywhnlzbj5804hwkk4rhzxzhzsycdf4v7a41"))))
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://hackage.haskell.org/package/miso/miso-"
+             version
+             ".tar.gz"))
+       (sha256
+        (base32
+         "1hkfcinwymrff8mmvywhnlzbj5804hwkk4rhzxzhzsycdf4v7a41"))))
     (build-system haskell-build-system)
     (inputs
-      `(("ghc-aeson" ,ghc-aeson)
-        ("ghc-http-api-data" ,ghc-http-api-data)
-        ("ghc-http-types" ,ghc-http-types)
-        ("ghc-network-uri" ,ghc-network-uri)
-        ("ghc-servant" ,ghc-servant)
-        ("ghc-lucid" ,ghc-lucid)
-        ("ghc-servant-lucid" ,ghc-servant-lucid)
-        ("ghc-vector" ,ghc-vector)))
+     `(("ghc-aeson" ,ghc-aeson)
+       ("ghc-http-api-data" ,ghc-http-api-data)
+       ("ghc-http-types" ,ghc-http-types)
+       ("ghc-network-uri" ,ghc-network-uri)
+       ("ghc-servant" ,ghc-servant)
+       ("ghc-lucid" ,ghc-lucid)
+       ("ghc-servant-lucid" ,ghc-servant-lucid)
+       ("ghc-vector" ,ghc-vector)))
     (arguments `(#:tests? #f))
     (home-page "https://github.com/dmjio/miso")
     (synopsis "A tasty Haskell front-end framework")
     (description
-      "Miso is a small, production-ready, \"isomorphic\" Haskell
+     "Miso is a small, production-ready, \"isomorphic\" Haskell
 front-end framework featuring a virtual-dom, recursive diffing / patching
 algorithm, event delegation, event batching, SVG, Server-sent events,
 Websockets, type-safe servant-style routing and an extensible
@@ -129,89 +129,44 @@ system via the Effect data type.  Miso makes heavy use of the GHCJS FFI
 and therefore has minimal dependencies.")
     (license license:bsd-3)))
 
-(define-public ghc-servant
-  (package
-    (name "ghc-servant")
-    (version "0.18.2")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (string-append
-               "https://hackage.haskell.org/package/servant/servant-"
-               version
-               ".tar.gz"))
-        (sha256
-          (base32
-            "18mfjj9za8g9rgj7a6j0ly6lf1ykfwrlpy3cf53lv1gxvb19gmk5"))))
-    (build-system haskell-build-system)
-    (inputs
-      `(("ghc-sop-core" ,ghc-sop-core)
-        ("ghc-http-api-data" ,ghc-http-api-data)
-        ("ghc-singleton-bool" ,ghc-singleton-bool)
-        ("ghc-base-compat" ,ghc-base-compat)
-        ("ghc-aeson" ,ghc-aeson)
-        ("ghc-attoparsec" ,ghc-attoparsec)
-        ("ghc-bifunctors" ,ghc-bifunctors)
-        ("ghc-case-insensitive" ,ghc-case-insensitive)
-        ("ghc-http-media" ,ghc-http-media)
-        ("ghc-http-types" ,ghc-http-types)
-        ("ghc-mmorph" ,ghc-mmorph)
-        ("ghc-network-uri" ,ghc-network-uri)
-        ("ghc-quickcheck" ,ghc-quickcheck)
-        ("ghc-string-conversions"
-         ,ghc-string-conversions)
-        ("ghc-tagged" ,ghc-tagged)
-        ("ghc-vault" ,ghc-vault)))
-    (arguments `(#:tests? #f))
-    (home-page "http://docs.servant.dev/")
-    (synopsis
-      "A family of combinators for defining webservices APIs")
-    (description
-      "A family of combinators for defining webservices APIs and serving
-them.  You can learn about the basics in the
-@url{http://docs.servant.dev/en/stable/tutorial/index.html, tutorial}.
-@url{https://github.com/haskell-servant/servant/blob/master/servant/CHANGELOG.md,
-CHANGELOG}")
-    (license license:bsd-3)))
-
 (define-public ghc-servant-client-core
   (package
     (name "ghc-servant-client-core")
     (version "0.18.2")
     (source
-      (origin
-        (method url-fetch)
-        (uri (string-append
-               "https://hackage.haskell.org/package/servant-client-core/servant-client-core-"
-               version
-               ".tar.gz"))
-        (sha256
-          (base32
-            "0b449c28z20sx98pc2d4p65jr3m9glsa47jjc2w4gf90jisl173r"))))
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://hackage.haskell.org/package/servant-client-core/servant-client-core-"
+             version
+             ".tar.gz"))
+       (sha256
+        (base32
+         "0b449c28z20sx98pc2d4p65jr3m9glsa47jjc2w4gf90jisl173r"))))
     (build-system haskell-build-system)
     (inputs
-      `(("ghc-servant" ,ghc-servant)
-        ("ghc-aeson" ,ghc-aeson)
-        ("ghc-base-compat" ,ghc-base-compat)
-        ("ghc-base64-bytestring" ,ghc-base64-bytestring)
-        ("ghc-exceptions" ,ghc-exceptions)
-        ("ghc-free" ,ghc-free)
-        ("ghc-http-media" ,ghc-http-media)
-        ("ghc-http-types" ,ghc-http-types)
-        ("ghc-network-uri" ,ghc-network-uri)
-        ("ghc-safe" ,ghc-safe)
-        ("ghc-sop-core" ,ghc-sop-core)))
+     `(("ghc-servant" ,ghc-servant)
+       ("ghc-aeson" ,ghc-aeson)
+       ("ghc-base-compat" ,ghc-base-compat)
+       ("ghc-base64-bytestring" ,ghc-base64-bytestring)
+       ("ghc-exceptions" ,ghc-exceptions)
+       ("ghc-free" ,ghc-free)
+       ("ghc-http-media" ,ghc-http-media)
+       ("ghc-http-types" ,ghc-http-types)
+       ("ghc-network-uri" ,ghc-network-uri)
+       ("ghc-safe" ,ghc-safe)
+       ("ghc-sop-core" ,ghc-sop-core)))
     (arguments
-      `(#:tests?
-        #f
-        #:cabal-revision
-        ("1"
-         "0vwj97h6x7zwvyx3ra09yhpi37mnn2kw5m27wnkzwwvk487swqxd")))
+     `(#:tests?
+       #f
+       #:cabal-revision
+       ("1"
+        "0vwj97h6x7zwvyx3ra09yhpi37mnn2kw5m27wnkzwwvk487swqxd")))
     (home-page "http://docs.servant.dev/")
     (synopsis
-      "Core functionality and class for client function generation for servant APIs")
+     "Core functionality and class for client function generation for servant APIs")
     (description
-      "This library provides backend-agnostic generation of client
+     "This library provides backend-agnostic generation of client
 functions.  For more information, see the README.")
     (license license:bsd-3)))
 
@@ -265,80 +220,28 @@ CHANGELOG}")
     (name "ghc-servant-lucid")
     (version "0.9.0.2")
     (source
-      (origin
-        (method url-fetch)
-        (uri (string-append
-               "https://hackage.haskell.org/package/servant-lucid/servant-lucid-"
-               version
-               ".tar.gz"))
-        (sha256
-          (base32
-            "0l68dffx746j3p2l5x59cj5cdng2dw6vjq5x5h44m0ccbsmlckpz"))))
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://hackage.haskell.org/package/servant-lucid/servant-lucid-"
+             version
+             ".tar.gz"))
+       (sha256
+        (base32
+         "0l68dffx746j3p2l5x59cj5cdng2dw6vjq5x5h44m0ccbsmlckpz"))))
     (build-system haskell-build-system)
     (inputs
-      `(("ghc-http-media" ,ghc-http-media)
-        ("ghc-lucid" ,ghc-lucid)
-        ("ghc-servant" ,ghc-servant)))
+     `(("ghc-http-media" ,ghc-http-media)
+       ("ghc-lucid" ,ghc-lucid)
+       ("ghc-servant" ,ghc-servant)))
     (arguments `(#:tests? #f))
     (home-page
-      "http://haskell-servant.readthedocs.io/")
+     "http://haskell-servant.readthedocs.io/")
     (synopsis "Servant support for lucid")
     (description
-      "Servant support for lucid.  @emph{HTML} content type which will use
+     "Servant support for lucid.  @emph{HTML} content type which will use
 @code{ToHtml} class.  Lucid.Servant uses @code{Link} rather than
 @code{Text} for safe @emph{href} attributes.")
-    (license license:bsd-3)))
-
-(define-public ghc-servant-server
-  (package
-    (name "ghc-servant-server")
-    (version "0.18.2")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (string-append
-               "https://hackage.haskell.org/package/servant-server/servant-server-"
-               version
-               ".tar.gz"))
-        (sha256
-          (base32
-            "05ricb3w1app6c094zwaq2jnqv53jpf4n89ffynm31dvf6h9qdih"))))
-    (build-system haskell-build-system)
-    (inputs
-      `(("ghc-servant" ,ghc-servant)
-        ("ghc-http-api-data" ,ghc-http-api-data)
-        ("ghc-base-compat" ,ghc-base-compat)
-        ("ghc-base64-bytestring" ,ghc-base64-bytestring)
-        ("ghc-exceptions" ,ghc-exceptions)
-        ("ghc-http-media" ,ghc-http-media)
-        ("ghc-http-types" ,ghc-http-types)
-        ("ghc-network-uri" ,ghc-network-uri)
-        ("ghc-monad-control" ,ghc-monad-control)
-        ("ghc-network" ,ghc-network)
-        ("ghc-sop-core" ,ghc-sop-core)
-        ("ghc-string-conversions"
-         ,ghc-string-conversions)
-        ("ghc-resourcet" ,ghc-resourcet)
-        ("ghc-tagged" ,ghc-tagged)
-        ("ghc-transformers-base" ,ghc-transformers-base)
-        ("ghc-wai" ,ghc-wai)
-        ("ghc-wai-app-static" ,ghc-wai-app-static)
-        ("ghc-word8" ,ghc-word8)
-        ("ghc-aeson" ,ghc-aeson)
-        ("ghc-warp" ,ghc-warp)))
-    (arguments `(#:tests? #f))
-    (home-page "http://docs.servant.dev/")
-    (synopsis
-      "A family of combinators for defining webservices APIs and serving them")
-    (description
-      "A family of combinators for defining webservices APIs and serving
-them.  You can learn about the basics in the
-@url{http://docs.servant.dev/en/stable/tutorial/index.html, tutorial}.
-@url{https://github.com/haskell-servant/servant/blob/master/servant-server/example/greet.hs,
-Here} is a runnable example, with comments, that defines a dummy API and
-implements a webserver that serves this API, using this package.
-@url{https://github.com/haskell-servant/servant/blob/master/servant-server/CHANGELOG.md,
-CHANGELOG}")
     (license license:bsd-3)))
 
 (define-public ghc-dec
@@ -346,21 +249,21 @@ CHANGELOG}")
     (name "ghc-dec")
     (version "0.0.4")
     (source
-      (origin
-        (method url-fetch)
-        (uri (string-append
-               "https://hackage.haskell.org/package/dec/dec-"
-               version
-               ".tar.gz"))
-        (sha256
-          (base32
-            "0yslffafmqfkvhcw2arpc53hfmn1788z85ss9lxnbclr29lbvzgc"))))
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://hackage.haskell.org/package/dec/dec-"
+             version
+             ".tar.gz"))
+       (sha256
+        (base32
+         "0yslffafmqfkvhcw2arpc53hfmn1788z85ss9lxnbclr29lbvzgc"))))
     (build-system haskell-build-system)
     (arguments `(#:tests? #f))
     (home-page "https://github.com/phadej/vec")
     (synopsis "Decidable propositions")
     (description
-      "This package provides a @code{Dec} type.
+     "This package provides a @code{Dec} type.
 @code{
 type Neg a = a -> Void
 
